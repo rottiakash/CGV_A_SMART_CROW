d3949d5dca51c4b25a73cb3847c4b0037ce9848b
rottiakash <rottiakash@gmail.com>
2020-04-07 10:50:49 +0530

    Added Triangle

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "line.hpp"
 #include "ellipse.hpp"
 #include "sector.hpp"
+#include "Triangle.hpp"
 #define FLAP_DOWN -180
 #define FLAP_UP 0
 class State
@@ -23,7 +24,7 @@ void init(void)
 void displayScene1(void)
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    /*     Circle circle;
+    /*         Circle circle;
     circle.setColor(0, 0, 0);
     circle.draw(500, 2000, 1000, 1);
     Line line;
@@ -31,10 +32,13 @@ void displayScene1(void)
     line.draw(1000, 1000, 2000, 2000, 1);
     Ellipse ellipse;
     ellipse.setColor(0, 0, 0);
-    ellipse.draw(1000, 1000, 1000, 500, 1); */
+    ellipse.draw(1000, 1000, 1000, 500, 1);
     Sector sector;
     sector.setColor(0, 0, 0);
-    sector.drawWing(State::flap, 1, 2000, 2000);
+    sector.drawWing(State::flap, 1, 2000, 2000); */
+    Triangle triangle;
+    triangle.setColor(0, 0, 0);
+    triangle.draw(1000, 1000, 3000, 1000, 2000, 3000, 1);
     glFlush();
     glutSwapBuffers();
 }
