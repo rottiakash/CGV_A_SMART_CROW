72ee9af7f75c0b1cc950cc39a0ae87875da29c9d
rottiakash <rottiakash@gmail.com>
2020-04-10 23:17:42 +0530

    Added cloud
    Co-authored-by: anandteerth1999 <anandmathad446@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <GLUT/glut.h>
+#include <iostream>
 #include "bird.hpp"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 #define FLAP_DOWN -180
 #define FLAP_UP 0
+unsigned int texture;
 class State
 {
 public:
@@ -26,7 +30,21 @@ void displayScene1(void)
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     Bird bird;
     bird.drawBird(State::flap, State::birdXpos, 2000);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glColor3f(1, 1, 1);
+    glEnable(GL_TEXTURE_2D);
+    glBegin(GL_QUADS);
+    glVertex3f(0, 0, 1);
+    glTexCoord2f(0, 0);
+    glVertex3f(0, 800, 1);
+    glTexCoord2f(0, 1);
+    glVertex3f(1500, 800, 1);
+    glTexCoord2f(1, 1);
+    glVertex3f(1500, 0, 1);
+    glTexCoord2f(1, 0);
+    glEnd();
     glFlush();
+    glDisable(GL_TEXTURE_2D);
     glutSwapBuffers();
 }
 void idle()
@@ -61,6 +79,30 @@ void timer(int value)
     glutTimerFunc(500, timer, 0);
     glutPostRedisplay();
 }
+
+void loadTexture(void)
+{
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    // set the texture wrapping/filtering options (on the currently bound texture object)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // load and generate the texture
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load("cloud.jpg", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        //glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+}
 int main(int argc, char **argv)
 {
 
@@ -72,6 +114,8 @@ int main(int argc, char **argv)
     glutMouseFunc(onClick);
     glutTimerFunc(1000, timer, 0);
     glEnable(GL_DEPTH_TEST);
+    glEnable(GL_TEXTURE_2D);
+    loadTexture();
     init();
     glutMainLoop();
 }
