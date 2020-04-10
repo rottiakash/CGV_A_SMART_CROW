a49aa3456370b4860281868cdd9951e6a6c8157d
rottiakash <rottiakash@gmail.com>
2020-04-10 19:43:11 +0530

    Bird Movement
    Co-authored-by: anandteerth1999 <anandmathad446@gmail.com>

--- main.cpp
+++ main.cpp
@@ -6,8 +6,12 @@ class State
 {
 public:
     static int flap;
+    static int birdXpos;
+    static int scene;
 };
 int State::flap = FLAP_DOWN;
+int State::birdXpos = 0;
+int State::scene = 0;
 void init(void)
 {
     glMatrixMode(GL_PROJECTION);
@@ -21,9 +25,32 @@ void displayScene1(void)
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     Bird bird;
-    bird.drawBird(State::flap, 2000, 2000);
+    bird.drawBird(State::flap, State::birdXpos, 2000);
     glFlush();
     glutSwapBuffers();
+}
+void idle()
+{
+    if (State::scene == 0)
+    {
+        if (State::birdXpos <= 2000)
+        {
+            State::birdXpos += 50;
+            glutPostRedisplay();
+        }
+        else
+        {
+            glutIdleFunc(NULL);
+        }
+    }
+}
+void onClick(int btn, int state, int x, int y)
+{
+    if (State::scene == 0)
+    {
+        if (btn == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+            glutIdleFunc(idle);
+    }
 }
 void timer(int value)
 {
@@ -42,6 +69,7 @@ int main(int argc, char **argv)
     glutInitWindowSize(1024, 1024);
     glutCreateWindow("A Smart Crow");
     glutDisplayFunc(displayScene1);
+    glutMouseFunc(onClick);
     glutTimerFunc(1000, timer, 0);
     glEnable(GL_DEPTH_TEST);
     init();
