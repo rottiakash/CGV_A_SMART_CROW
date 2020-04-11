e809d6475941dd7de80d228440f7bff1dee92c62
rottiakash <rottiakash@gmail.com>
2020-04-11 22:55:12 +0530

    Added Keyboard function
    Co-authored-by: anandteerth1999 <anandmathad446@gmail.com>

--- main.cpp
+++ main.cpp
@@ -2,9 +2,11 @@
 #include <iostream>
 #include "bird.hpp"
 #define STB_IMAGE_IMPLEMENTATION
+#define SPACEBAR 32
 #include "stb_image.h"
 #define FLAP_DOWN -180
 #define FLAP_UP 0
+void drawtext(float, float, char *);
 unsigned int bg1;
 class State
 {
@@ -13,11 +15,13 @@ public:
     static int birdXpos;
     static int scene;
     static bool displayCloudS1;
+    static bool moveBird;
 };
 int State::flap = FLAP_DOWN;
 int State::birdXpos = 0;
 int State::scene = 0;
 bool State::displayCloudS1 = false;
+bool State::moveBird = false;
 void init(void)
 {
     glMatrixMode(GL_PROJECTION);
@@ -34,7 +38,11 @@ void displayScene1(void)
     bird.drawBird(State::flap, State::birdXpos, 2000);
     glDisable(GL_TEXTURE_2D);
     if (State::displayCloudS1)
+    {
         bird.cloud(3200, 2500, "Its a Sunny day", "I'm Thirsty");
+        glColor3f(1, 1, 1);
+        drawtext(2100, 430, "Press SPACEBAR to continue");
+    }
     glEnable(GL_TEXTURE_2D);
     glColor3f(1, 1, 1);
     glBindTexture(GL_TEXTURE_2D, bg1);
@@ -52,6 +60,23 @@ void displayScene1(void)
     glDisable(GL_TEXTURE_2D);
     glutSwapBuffers();
 }
+void displayScene2(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glBegin(GL_LINES);
+    glColor3f(0, 0, 0);
+    glVertex3f(1000, 1000, 1);
+    glVertex3f(2000, 2000, 1);
+    glEnd();
+    glFlush();
+    glutSwapBuffers();
+}
+void drawtext(float x, float y, char *s)
+{
+    glRasterPos2f(x, y);
+    for (int i = 0; s[i] != '\0'; i++)
+        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, s[i]);
+}
 void idle()
 {
     if (State::scene == 0)
@@ -64,10 +89,31 @@ void idle()
         else
         {
             State::displayCloudS1 = true;
+            glFlush();
             glutIdleFunc(NULL);
         }
     }
 }
+
+void moveBird(void)
+{
+    if (State::scene == 0)
+    {
+        if (State::birdXpos <= 5000)
+        {
+            State::birdXpos += 50;
+            glutPostRedisplay();
+        }
+        else
+        {
+            glutIdleFunc(NULL);
+            State::scene = 1;
+            glutDisplayFunc(displayScene2);
+            glutPostRedisplay();
+        }
+    }
+}
+
 void onClick(int btn, int state, int x, int y)
 {
     if (State::scene == 0)
@@ -76,6 +122,17 @@ void onClick(int btn, int state, int x, int y)
             glutIdleFunc(idle);
     }
 }
+
+void keyboard(unsigned char key, int x, int y)
+{
+    if (State::displayCloudS1)
+        if (key == SPACEBAR)
+        {
+            State::displayCloudS1 = false;
+            glutIdleFunc(moveBird);
+        }
+}
+
 void timer(int value)
 {
     if (State::flap == FLAP_DOWN)
@@ -118,6 +175,7 @@ int main(int argc, char **argv)
     glutCreateWindow("A Smart Crow");
     glutDisplayFunc(displayScene1);
     glutMouseFunc(onClick);
+    glutKeyboardFunc(keyboard);
     glutTimerFunc(1000, timer, 0);
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_TEXTURE_2D);
